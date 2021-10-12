51b625fdf30246e56f1f3cf83ba21e6bfa5dd812
Denis Maliutenko <mrfix@MacBook-Pro-Admin.local>
2021-10-12 13:01:35 +0300

    Add simple AI

---
+++ Source/DD/DDAI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DDGASBaseCharacter.h"
+#include "DDAI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DD_API ADDAI : public ADDGASBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
