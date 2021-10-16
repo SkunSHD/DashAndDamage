cee4aa5ef744773198b092b66b57fff575ca9633
den <den@DESKTOP-6SNMKIF>
2021-10-16 23:08:57 +0300

    Change folders structure

--- Source/DD/DDGameMode.cpp
+++ Source/DD/DDGameMode.cpp
@@ -8,7 +8,7 @@
 ADDGameMode::ADDGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
